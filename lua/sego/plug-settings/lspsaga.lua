bc762ae85d033d99a46cb9df0cd6d3e22ea2eb1b
Fernando Segovia <fernandosegovia25@gmail.com>
2022-11-29 14:55:39 -0600

    Add Lspsaga

---
+++ lua/sego/plug-settings/lspsaga.lua
@@ -0,0 +1,14 @@
+local saga = require('lspsaga')
+
+saga.init_lsp_saga({
+  code_action_lightbulb = {
+    enable = false,
+    enable_in_insert = true,
+    cache_code_action = true,
+    sign = true,
+    update_time = 150,
+    sign_priority = 20,
+    virtual_text = true,
+  },
+
+})
