18e4a2a929c3464b15f356371163f96bb09cca79
Fernando Segovia <fernandosegovia25@gmail.com>
2023-01-04 18:04:08 -0600

    Add syntastic

---
+++ lua/sego/plug-settings/syntastic.lua
@@ -0,0 +1,8 @@
+vim.g.syntastic_java_checkers = {'checkstyle'}
+vim.g.syntastic_java_checkstyle_classpath = '~/.local/share/checkstyle/checkstyle-10.5.0-all.jar'
+vim.g.syntastic_java_checkstyle_conf_file = '~/.local/share/checkstyle/checkstyle.xml'
+
+vim.g.syntastic_always_populate_loc_list = 1
+vim.g.syntastic_auto_loc_list = 0
+vim.g.syntastic_check_on_open = 1
+vim.g.syntastic_check_on_wq = 0
